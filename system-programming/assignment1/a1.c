04274f0ed2c5be9e307beebe5d1e571110538b1c
Sunwoo Kim <seng001127@soongsil.ac.kr>
2024-03-27 13:24:02 +0900

    system programming 과제1

---
+++ system-programming/assignment1/a1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void dump_memory(const void *mem, size_t len)
+{
+    const char *buffer = mem;
+    size_t i = 0;
+    for(i = 0; i < len; i++)
+    {
+        if(i > 0 && i % 8 == 0) printf("\n");
+        printf("%02x ", buffer[i] & 0xff);
+    }
+    if(i > 1 && i % 8 != 1) puts("");
+}
+
+int main()
+{
+    char c = 0x80;
+    int i = c;
+    dump_memory(&i, sizeof(i));
+    return 0;
+}
