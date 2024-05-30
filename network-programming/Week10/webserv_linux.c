6320dc8baf18d8c0a30d37d742d482617a733ba8
Sunwoo Kim <seng001127@soongsil.ac.kr>
2024-05-30 17:44:21 +0900

    깨진 값 수정

--- network-programming/Week10/webserv_linux.c
+++ network-programming/Week10/webserv_linux.c
@@ -9,11 +9,11 @@
 #define BUF_SIZE 1024
 #define SMALL_BUF 100
 
-void* request_handler(void* arg);
-void send_data(FILE* fp, char* ct, char* file_name);
-char* content_type(char* file);
-void send_error(FILE* fp);
-void error_handling(char* message);
+void *request_handler(void *arg);
+void send_data(FILE *fp, char *ct, char *file_name);
+char *content_type(char *file);
+void send_error(FILE *fp);
+void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
@@ -21,28 +21,29 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_adr, clnt_adr;
 	int clnt_adr_size;
 	char buf[BUF_SIZE];
-	pthread_t t_id;	
-	if(argc!=2) {
+	pthread_t t_id;
+	if (argc != 2)
+	{
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0); // IPv4 TCP
 	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_adr.sin_port = htons(atoi(argv[1]));
-	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+	if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
 		error_handling("bind() error");
-	if(listen(serv_sock, 20)==-1)
+	if (listen(serv_sock, 20) == -1)
 		error_handling("listen() error");
 
-	while(1)
+	while (1)
 	{
-		clnt_adr_size=sizeof(clnt_adr);
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_size);
-		printf("Connection Request : %s:%d\n", 
-			inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port));
+		clnt_adr_size = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_size);
+		printf("Connection Request : %s:%d\n",
+			   inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port));
 		pthread_create(&t_id, NULL, request_handler, &clnt_sock);
 		pthread_detach(t_id);
 	}
@@ -50,68 +51,66 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void* request_handler(void *arg)
+void *request_handler(void *arg)
 {
-	int clnt_sock=*((int*)arg);
+	int clnt_sock = *((int *)arg);
 	char req_line[SMALL_BUF];
-	FILE* clnt_read;
-	FILE* clnt_write;
-	
+	FILE *clnt_read;
+	FILE *clnt_write;
+
 	char method[10];
 	char ct[15];
 	char file_name[30];
-  
-	clnt_read=fdopen(clnt_sock, "r");
-	clnt_write=fdopen(dup(clnt_sock), "w");
-	fgets(req_line, SMALL_BUF, clnt_read);	
-	if(strstr(req_line, "HTTP/")==NULL)
+
+	clnt_read = fdopen(clnt_sock, "r");
+	clnt_write = fdopen(dup(clnt_sock), "w");
+	fgets(req_line, SMALL_BUF, clnt_read);
+	if (strstr(req_line, "HTTP/") == NULL)
 	{
 		send_error(clnt_write);
 		fclose(clnt_read);
 		fclose(clnt_write);
 		return;
-	 }
-	
+	}
+
 	strcpy(method, strtok(req_line, " /"));
 	strcpy(file_name, strtok(NULL, " /"));
 	strcpy(ct, content_type(file_name));
-	if(strcmp(method, "GET")!=0)
+	if (strcmp(method, "GET") != 0)
 	{
 		send_error(clnt_write);
 		fclose(clnt_read);
 		fclose(clnt_write);
 		return;
-	 }
+	}
 
 	fclose(clnt_read);
-	send_data(clnt_write, ct, file_name); 
+	send_data(clnt_write, ct, file_name);
 }
 
-void send_data(FILE* fp, char* ct, char* file_name)
+void send_data(FILE *fp, char *ct, char *file_name)
 {
-	char protocol[]="HTTP/1.0 200 OK\r\n";
-	char server[]="Server:Linux Web Server \r\n";
-	char cnt_len[]="Content-length:2048\r\n";
+	char protocol[] = "HTTP/1.0 200 OK\r\n";
+	char server[] = "Server:Linux Web Server \r\n";
+	char cnt_len[] = "Content-length:2048\r\n";
 	char cnt_type[SMALL_BUF];
 	char buf[BUF_SIZE];
-	FILE* send_file;
-	
+	FILE *send_file;
+
 	sprintf(cnt_type, "Content-type:%s\r\n\r\n", ct);
-	send_file=fopen(file_name, "r");
-	if(send_file==NULL)
+	send_file = fopen(file_name, "r");
+	if (send_file == NULL)
 	{
 		send_error(fp);
 		return;
 	}
 
-	/* 헤더 정보 전송 */
 	fputs(protocol, fp);
 	fputs(server, fp);
 	fputs(cnt_len, fp);
 	fputs(cnt_type, fp);
 
-	/* 요청 데이터 전송 */
-	while(fgets(buf, BUF_SIZE, send_file)!=NULL) 
+	while (fgets(buf, BUF_SIZE, send_file) != NULL)
 	{
 		fputs(buf, fp);
 		fflush(fp);
@@ -120,29 +119,29 @@ void send_data(FILE* fp, char* ct, char* file_name)
 	fclose(fp);
 }
 
-char* content_type(char* file)
+char *content_type(char *file)
 {
 	char extension[SMALL_BUF];
 	char file_name[SMALL_BUF];
 	strcpy(file_name, file);
 	strtok(file_name, ".");
 	strcpy(extension, strtok(NULL, "."));
-	
-	if(!strcmp(extension, "html")||!strcmp(extension, "htm")) 
+
+	if (!strcmp(extension, "html") || !strcmp(extension, "htm"))
 		return "text/html";
 	else
 		return "text/plain";
 }
 
-void send_error(FILE* fp)
-{	
-	char protocol[]="HTTP/1.0 400 Bad Request\r\n";
-	char server[]="Server:Linux Web Server \r\n";
-	char cnt_len[]="Content-length:2048\r\n";
-	char cnt_type[]="Content-type:text/html\r\n\r\n";
-	char content[]="<html><head><title>NETWORK</title></head>"
-	       "<body><font size=+5><br>오류 발생! 요청 파일명 및 요청 방식 확인!"
-		   "</font></body></html>";
+void send_error(FILE *fp)
+{
+	char protocol[] = "HTTP/1.0 400 Bad Request\r\n";
+	char server[] = "Server:Linux Web Server \r\n";
+	char cnt_len[] = "Content-length:2048\r\n";
+	char cnt_type[] = "Content-type:text/html\r\n\r\n";
+	char content[] = "<html><head><title>NETWORK</title></head>"
+					 "<body><font size=+5><br>Hello, <i>Worlds!</i>"
+					 "</font></body></html>";
 
 	fputs(protocol, fp);
 	fputs(server, fp);
@@ -151,7 +150,7 @@ void send_error(FILE* fp)
 	fflush(fp);
 }
 
-void error_handling(char* message)
+void error_handling(char *message)
 {
 	fputs(message, stderr);
 	fputc('\n', stderr);
